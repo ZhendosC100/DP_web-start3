5799a8f7ee8d52978fca90256df3f296fc17a7b3
Yauhen Tretsyak <zhendos@tut.by>
2019-06-15 19:10:51 +0200

    added aside shadow, some parts of navbar section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 $(document).ready( function(){
     let basket = $('.basket'),
+        shadow = $('.aside_shadow'),
         close = $('.close');
     $('.modal_basket').on('click', function(e){
         e.preventDefault();
         basket.show();
+        shadow.show();
     });
 
     close.on('click', function(){
         basket.hide();
+        shadow.hide();
     });
 });

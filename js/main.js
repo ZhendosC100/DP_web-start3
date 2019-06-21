3105db669f24f7d57e6a8c3d551a6b698794a8e3
Yauhen Tretsyak <zhendos@tut.by>
2019-06-21 13:54:53 +0200

    added page Product, pp 1170px slider, info sections

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 $(document).ready( function(){
     let basket = $('.basket'),
+        productPage = $('.sale__wrapper_box'),
         shadow = $('.aside_shadow'),
         close = $('.close');
     $('.price_buy_btn__img').on('click', function(e){
         e.preventDefault();
         basket.show();
         shadow.show();
+    });
+
+    productPage.on('click', function(){
+        location.href="product.html";
     });
 
     close.on('click', function(){

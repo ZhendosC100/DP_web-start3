3584aec03872b445dc281940ab474422f05d3d11
Yauhen Tretsyak <zhendos@tut.by>
2019-06-22 18:18:12 +0200

    Added menu full responsive

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 $(document).ready( function(){
     let basket = $('.basket'),
-        productPage = $('.sale__wrapper_box'),
+        productPage = $('.sale__img'),
         shadow = $('.aside_shadow'),
         close = $('.close');
     $('.price_buy_btn__img').on('click', function(e){
         e.preventDefault();
         basket.show();
         shadow.show();
+    });
+
+    $('.menu__hamburger_img').on('click', function(){
+
+        if ($(window).width() <= '950'){
+            $('.humburger-section').toggleClass('active_menu');
+        }
+        
     });
 
     productPage.on('click', function(){

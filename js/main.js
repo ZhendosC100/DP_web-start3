9bb5d148b74b7ed5c826276adf7fa31876da4e20
Yauhen Tretsyak <zhendos@tut.by>
2019-06-23 01:20:24 +0200

    modal question

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready( function(){
     let basket = $('.basket'),
         productPage = $('.sale__img'),
         shadow = $('.aside_shadow'),
+        question = $('.question'),
         close = $('.close');
     $('.price_buy_btn__img').on('click', function(e){
         e.preventDefault();
@@ -17,12 +18,18 @@ $(document).ready( function(){
         
     });
 
+    $('.contact__btn').on('click', function(){
+        question.addClass('active_question');
+        shadow.show();
+    });
+
     productPage.on('click', function(){
         location.href="product.html";
     });
 
     close.on('click', function(){
         basket.hide();
+        question.removeClass('active_question');
         shadow.hide();
     });
 });
